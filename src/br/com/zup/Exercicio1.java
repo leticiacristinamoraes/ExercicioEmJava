e413f1e2c024e29664d296b960451206275f7fcf
Letícia Cristina França Moraes dos Santos <leticiacristinafmds@gmail.com>
2021-08-24 14:06:07 -0300

    Testando print com concatenação

--- src/br/com/zup/Exercicio1.java
+++ src/br/com/zup/Exercicio1.java
@@ -6,11 +6,16 @@ public class Exercicio1 {
 
         int idadeDoUsuario = 18;
         String nomeDoUsuario = "Andre";
-        System.out.print("Olá, meu nome é ");
-        System.out.print(nomeDoUsuario);
-        System.out.print("e minha idade é ");
-        System.out.print(idadeDoUsuario);
-        System.out.print(" anos");
+
+        // Printando as variáveis declaradas
+        //System.out.print("Olá, meu nome é ");
+        //System.out.print(nomeDoUsuario);
+        //System.out.print("e minha idade é ");
+        //System.out.print(idadeDoUsuario);
+        //System.out.print(" anos");//
+
+        // Printando de forma concatenada as variáveis declaradas
+        System.out.print("Olá, meu nome é " + nomeDoUsuario);
 
 
 
