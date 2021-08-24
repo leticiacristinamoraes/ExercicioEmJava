62e6ac8b072506f9e605c93c4881548de2bfbb54
Letícia Cristina França Moraes dos Santos <leticiacristinafmds@gmail.com>
2021-08-24 14:08:36 -0300

    Testando de novo hehe

--- src/br/com/zup/Exercicio1.java
+++ src/br/com/zup/Exercicio1.java
@@ -5,14 +5,14 @@ public class Exercicio1 {
     public static void main(String[] args) {
 
         int idadeDoUsuario = 18;
-        String nomeDoUsuario = "Andre";
+        String nomeDoUsuario = "Xablau";
 
         // Printando as variáveis declaradas
-        //System.out.print("Olá, meu nome é ");
-        //System.out.print(nomeDoUsuario);
-        //System.out.print("e minha idade é ");
-        //System.out.print(idadeDoUsuario);
-        //System.out.print(" anos");//
+        System.out.print("Olá, meu nome é ");
+        System.out.print(nomeDoUsuario);
+        System.out.print("e minha idade é ");
+        System.out.print(idadeDoUsuario);
+        System.out.println(" anos");
 
         // Printando de forma concatenada as variáveis declaradas
         System.out.print("Olá, meu nome é " + nomeDoUsuario);
