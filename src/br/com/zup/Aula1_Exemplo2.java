ec3b13094d2630a5a01970de5319492271c8e478
Letícia Cristina França Moraes dos Santos <leticiacristinafmds@gmail.com>
2021-08-24 14:56:42 -0300

    alteração das variáveis numero1 e numero2

--- src/br/com/zup/Aula1_Exemplo2.java
+++ src/br/com/zup/Aula1_Exemplo2.java
@@ -3,8 +3,8 @@ package br.com.zup;
 public class Aula1_Exemplo2 {
     public static void main(String[] args) {
         // Armazenar os valores em memória
-        int numero1 = 20;
-        int numero2 = 30;
+        int numero1 = 100;
+        int numero2 = 20;
 
         // Criando operação de soma
         int resultadoDaSoma = numero1 + numero2;
