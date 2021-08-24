4f2219216150fced2959063dffbc2dc59740decd
Letícia Cristina França Moraes dos Santos <leticiacristinafmds@gmail.com>
2021-08-24 13:59:26 -0300

    Primeiro Exemplo do dia 24/08/2021

---
+++ src/br/com/zup/Exercicio1.java
@@ -0,0 +1,18 @@
+package br.com.zup;
+
+public class Exercicio1 {
+
+    public static void main(String[] args) {
+
+        int idadeDoUsuario = 18;
+        String nomeDoUsuario = "Andre";
+        System.out.print("Olá, meu nome é ");
+        System.out.print(nomeDoUsuario);
+        System.out.print("e minha idade é ");
+        System.out.print(idadeDoUsuario);
+        System.out.print(" anos");
+
+
+
+    }
+}
