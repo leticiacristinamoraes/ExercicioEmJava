e80ef583022b9a4f71b4d06a9ff17e825666eb7a
Letícia Cristina França Moraes dos Santos <leticiacristinafmds@gmail.com>
2021-08-24 14:46:19 -0300

    Adicionando a função de subtração

--- src/br/com/zup/Aula1_Exemplo2.java
+++ src/br/com/zup/Aula1_Exemplo2.java
@@ -1,2 +1,21 @@
-package br.com.zup;public class Aula1_Exemplo2 {
+package br.com.zup;
+
+public class Aula1_Exemplo2 {
+    public static void main(String[] args) {
+        // Armazenar os valores em memória
+        int numero1 = 20;
+        int numero2 = 30;
+
+        // Criando operação de soma
+        int resultadoDaSoma = numero1 + numero2;
+
+        // Criando operação de subtração
+        int resultadoDaSubtração = numero1 - numero2;
+
+        // Exibindo a operação de soma para o usuário
+        System.out.println("O resultado da soma é " + resultadoDaSoma);
+
+        // Exibindo a operação da subtração para o usuário
+        System.out.println("O resultado da subtração é " + resultadoDaSubtração);
+    }
 }
