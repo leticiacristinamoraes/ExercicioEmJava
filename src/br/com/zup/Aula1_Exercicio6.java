fc9d87f64d04eb21fb88d0a87b2ee55a2c8e8e5a
Letícia Cristina França Moraes dos Santos <leticiacristinafmds@gmail.com>
2021-08-25 14:17:09 -0300

    Adição das variáveis com os descontos que incidem sobre o salário bruto

--- src/br/com/zup/Aula1_Exercicio6.java
+++ src/br/com/zup/Aula1_Exercicio6.java
@@ -10,5 +10,10 @@ public class Aula1_Exercicio6 {
         // Valor do salário bruto
         double salarioBruto = valorHoraTrabalhada * horasTrabalhadas;
 
+        // Descontos incidentes sobre o salário bruto
+        double impostoDeRenda = 0.11 * salarioBruto;
+        double inss = 0.08 * salarioBruto;
+        double sindicato = 0.05 * salarioBruto;
+
     }
 }
