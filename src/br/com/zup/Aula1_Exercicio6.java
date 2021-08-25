83417b14e0a7c3f5aeba3900b294f32e3ae8edc2
Letícia Cristina França Moraes dos Santos <leticiacristinafmds@gmail.com>
2021-08-25 20:52:38 -0300

    Adicionado o scanner no exercício 6

--- src/br/com/zup/Aula1_Exercicio6.java
+++ src/br/com/zup/Aula1_Exercicio6.java
@@ -7,25 +7,24 @@ public class Aula1_Exercicio6 {
 
         Scanner leitor = new Scanner(System.in);
 
-        // Variáveis com o valor das horas trabalhas e o número de horas trabalhadas
-        double valorHoraTrabalhada = leitor.nextDouble();
-        double horasTrabalhadas = leitor.nextDouble();
+        // Variável com o valor das horas trabalhas
+        double valorHoraTrabalhada = 30;
 
-        // Valor do salário bruto
+        // Exibir ao usuário o valor do salário bruto, valor pago ao inss, valor pago ao sindicato e o salário líquido
+        System.out.println("Por favor insira o número de horas trabalhadas nesse mês:");
+        double horasTrabalhadas = leitor.nextDouble();
         double salarioBruto = valorHoraTrabalhada * horasTrabalhadas;
 
-        // Descontos incidentes sobre o salário bruto
+        // Cálculo dos impostos incidentes sobre o salário bruto
         double impostoDeRenda = 0.11 * salarioBruto;
         double inss = 0.08 * salarioBruto;
         double sindicato = 0.05 * salarioBruto;
 
-        // Soma dos descontos do salário
+        // Cálculo do salário líquido após os descontos
         double descontos = impostoDeRenda+inss+sindicato;
         double salarioLiquido = salarioBruto - descontos;
 
 
-
-        // Exibir ao usuário o valor do salário bruto, valor pago ao inss, valor pago ao sindicato e o salário líquido
         System.out.println("Seu salário bruto é de R$" + salarioBruto + " esse mês.");
         System.out.println("O valor descontado referente ao INSS foi de R$" + inss);
         System.out.println("O valor descontado do salário que foi pago ao sindicato foi de R$" + sindicato);
