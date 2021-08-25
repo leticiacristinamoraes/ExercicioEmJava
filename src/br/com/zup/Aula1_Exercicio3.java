34ca3a0dc6a6f94a3974b7bf4a5d1955a083947f
Letícia Cristina França Moraes dos Santos <leticiacristinafmds@gmail.com>
2021-08-25 10:55:07 -0300

    Exibindo a mensagem para o usuário

--- src/br/com/zup/Aula1_Exercicio3.java
+++ src/br/com/zup/Aula1_Exercicio3.java
@@ -5,10 +5,13 @@ public class Aula1_Exercicio3 {
         // Variável que recebe a informação de quantos metros serão convertidos
         // e qual é o valor de cada metro em centímetros
         float metros = 4.7F;
-        float metrosEmCentímetros = 1000;
+        float metrosEmCentímetros = 1000F;
 
         // Variável que faça a conversão de metros em centímetros
         float conversao = metros * metrosEmCentímetros;
+
+        // Exibindo a mensagem de conversão
+        System.out.println("Esses " + metros + " metros equivalem a " + conversao + " centímetros");
 
     }
 }
