29e8b2521fee2a62da79b2a4527eab0b8db6bb5d
Letícia Cristina França Moraes dos Santos <leticiacristinafmds@gmail.com>
2021-08-25 14:31:59 -0300

    Exibindo ao usuário as informações sobre o salário bruto, valor pago ao inss, valor pago ao sindicato e o salário líquido depois de todos os descontos

--- src/br/com/zup/Aula1_Exercicio6.java
+++ src/br/com/zup/Aula1_Exercicio6.java
@@ -19,5 +19,11 @@ public class Aula1_Exercicio6 {
         double descontos = impostoDeRenda+inss+sindicato;
         double salarioLiquido = salarioBruto - descontos;
 
+        // Exibir ao usuário o valor do salário bruto, valor pago ao inss, valor pago ao sindicato e o salário líquido
+        System.out.println("Seu salário bruto é de R$" + salarioBruto + " esse mês.");
+        System.out.println("O valor descontado referente ao INSS foi de R$" + inss);
+        System.out.println("O valor descontado do salário que foi pago ao sindicato foi de R$" + sindicato);
+        System.out.println("Seu salário líquido após todos os descontos é de R$" + salarioLiquido);
+
     }
 }
