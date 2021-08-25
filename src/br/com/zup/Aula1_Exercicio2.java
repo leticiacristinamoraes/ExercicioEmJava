f65d87b98d152112b76f95c3ff3217a68e5e8f6a
Letícia Cristina França Moraes dos Santos <leticiacristinafmds@gmail.com>
2021-08-25 10:29:07 -0300

    Criando variáveis para as 4 notas bimestrais

---
+++ src/br/com/zup/Aula1_Exercicio2.java
@@ -0,0 +1,13 @@
+package br.com.zup;
+
+public class Aula1_Exercicio2 {
+    public static void main(String[] args) {
+        // Notas bimestrais recebidas
+        float nota1 = 8.5F;
+        float nota2 = 7.7F;
+        float nota3 = 9.2F;
+        float nota4 = 9;
+
+
+    }
+}
