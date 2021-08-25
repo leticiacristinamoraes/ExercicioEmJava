fee6aa68f1d8107f25388247e15eff948aeb765b
Letícia Cristina França Moraes dos Santos <leticiacristinafmds@gmail.com>
2021-08-25 10:19:56 -0300

    Primeiro Exercício

---
+++ src/br/com/zup/Aula1_Exercicio1.java
@@ -0,0 +1,8 @@
+package br.com.zup;
+
+public class Aula1_Exercicio1 {
+    public static void main(String[] args) {
+        String mensagem = "Alô Mundo!";
+        System.out.println(mensagem);
+    }
+}
