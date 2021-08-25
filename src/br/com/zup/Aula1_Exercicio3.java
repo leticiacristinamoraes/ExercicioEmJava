b51ba02afeb589022fc0f47fb732724c393cc4a6
Letícia Cristina França Moraes dos Santos <leticiacristinafmds@gmail.com>
2021-08-25 10:46:20 -0300

    Criação das variáveis que contém os metros e o valor de cada metro em centímetros

---
+++ src/br/com/zup/Aula1_Exercicio3.java
@@ -0,0 +1,11 @@
+package br.com.zup;
+
+public class Aula1_Exercicio3 {
+    public static void main(String[] args) {
+        // Variável que recebe a informação de quantos metros serão convertidos
+        // e qual é o valor de cada metro em centímetros
+        float metros = 4.7F;
+        float metrosEmCentímetros = 1000;
+
+    }
+}
