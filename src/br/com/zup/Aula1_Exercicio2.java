033c494782db8581e847e79ed41268a454246752
Letícia Cristina França Moraes dos Santos <leticiacristinafmds@gmail.com>
2021-08-25 16:30:20 -0300

    Commitão da madrugada kkkkkk adicionado o scanner no código

--- src/br/com/zup/Aula1_Exercicio2.java
+++ src/br/com/zup/Aula1_Exercicio2.java
@@ -1,18 +1,27 @@
 package br.com.zup;
 
+import java.util.Scanner;
+
 public class Aula1_Exercicio2 {
     public static void main(String[] args) {
         // Notas bimestrais recebidas
-        float nota1 = 8.5F;
-        float nota2 = 7.7F;
-        float nota3 = 9.2F;
-        float nota4 = 9;
 
-        // Criando variáveis para somar as notas bimestrais e fazer a média aritmética
-        float somaDasNotas = nota1 + nota2 +nota3 + nota4;
-        float mediaDasNotas = somaDasNotas/4;
+        Scanner leitor = new Scanner(System.in);
 
         // Exibir a mensagem para o usuário sobre a média aritmética
+        System.out.println("Por favor digite a primeira nota:");
+        double nota1 = leitor.nextDouble();
+        System.out.println("Por favor digite a segunda nota:");
+        double nota2 = leitor.nextDouble();
+        System.out.println("Por favor digite a terceira nota:");
+        double nota3 = leitor.nextDouble();
+        System.out.println("Por favor digite a quarta nota:");
+        double nota4 = leitor.nextDouble();
+
+
+        double somaDasNotas = nota1 + nota2 +nota3 + nota4;
+        double mediaDasNotas = somaDasNotas/4;
+
         System.out.println("Olá, sua média nesse bimestre é " + mediaDasNotas + " Continue se dedicando em seus estudos!");
 
 
