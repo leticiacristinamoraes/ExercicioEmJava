02edec7f49c90a137e0eda39ad546cded21a9ebd
Letícia Cristina França Moraes dos Santos <leticiacristinafmds@gmail.com>
2021-08-25 13:42:19 -0300

    Criação da variável que contém a temperatura em graus Fahrenheit

---
+++ src/br/com/zup/Aula1_Exercicio5.java
@@ -0,0 +1,9 @@
+package br.com.zup;
+
+public class Aula1_Exercicio5 {
+    public static void main(String[] args) {
+
+        // Variável com a temperatura em fahrenheit
+        double temperaturaEmFahrenheit = 50;
+    }
+}
