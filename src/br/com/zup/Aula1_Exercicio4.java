41afd091509178a212a76945e489f0c46523d4e4
Letícia Cristina França Moraes dos Santos <leticiacristinafmds@gmail.com>
2021-08-25 20:46:35 -0300

    Adicionado o scanner no exercício 4

--- src/br/com/zup/Aula1_Exercicio4.java
+++ src/br/com/zup/Aula1_Exercicio4.java
@@ -1,15 +1,23 @@
 package br.com.zup;
 
+import java.util.Scanner;
+
 public class Aula1_Exercicio4 {
     public static void main(String[] args) {
+
+        Scanner leitor = new Scanner(System.in);
         // Variáveis com o valor da hora trabalhada e o número de horas trabalhadas durante o mês
-        double valorHoraTrabalhada = 35.70;
-        double horasTrabalhadas = 105.5;
+
 
         // Salário a receber em função das horas trabalhadas
-        double salarioAReceber = valorHoraTrabalhada * horasTrabalhadas;
 
         // Exibir ao usuário o número de horas trabalhadas durante o mês e seu respectivo salário
+        System.out.println("Por favor insira o valor da hora trabalhada:");
+        double valorHoraTrabalhada = leitor.nextDouble();
+        System.out.println("Por favor insira o total de horas trabalhadas no mês:");
+        double horasTrabalhadas = leitor.nextDouble();
+        double salarioAReceber = valorHoraTrabalhada * horasTrabalhadas;
+
         System.out.println("Você trabalhou " + horasTrabalhadas + " horas neste mês.");
         System.out.println("Seu salário esse mês será: R$" + salarioAReceber);
 
